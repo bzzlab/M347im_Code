fab1d0b981799ed3571b1fa5bd0b66d090928aa1
dgaravaldi <daniel.garavaldi@gmail.com>
2024-05-15 14:58:04 +0200

    02/Intro

---
+++ 02/01_Intro/02_setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# 'import' 0x_param.conf
+source 02_param.conf #??
+# Build a dockerfile with tag -t
+docker build -t "$image" -f "$file" . #??
+# List images
+docker image ls #??
+# Run image with and name it (see variable)
+docker run -itd --rm --name "$container" -p 8080:80 "$image" #??
+# Test container
+curl -X GET http://localhost:8080 #??
---
+++ 02/01_Intro/03a_teardown.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#
+# 'import' 0x_param.conf
+source 03a_param.conf #??
+# 'import' teardown base script
+source td-base.sh 2 #??
