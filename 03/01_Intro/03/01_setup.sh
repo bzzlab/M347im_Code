40986052cd6bf8cb6dc65df27dd364f239e1a878
dgaravaldi <daniel.garavaldi@gmail.com>
2025-05-21 09:37:07 +0200

    03 intro

---
+++ 03/01_Intro/03/01_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+### Steps to build and run 0x_dockerfile
+PARAMS='01_params.conf'
+if [ ! -f $PARAMS ]; then  #??
+    echo $PARAMS does not exist! #??
+    exit 2  #??
+  fi  #??
+source $PARAMS #??
+# Build a dockerfile with tag -t
+docker build -t "$image" -f "$file" . #??
+# List images
+docker image ls #??
+
+# Run the Docker container with auto-restart policy
+docker run -d --name "$container" -p 3000:3000 --restart always "$image" #??
+
+
---
+++ 03/01_Intro/03/td-base.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+if test $# -lt 1; then
+  printf "%s\n" \
+    "Error: Provide at least 1 argument:" \
+    "(1): remove all, (2): remove only image" \
+    "Exit script."
+  exit 1
+fi
+
+case $1 in
+1)
+  printf "Stop and remove container %s and image %s" ${image} ${container}
+  # stop container
+  docker stop "$container"  #??
+  sleep 3
+  # Remove container  #??
+  id=$(docker ps -a |grep ${image}|xargs|cut -d' ' -f1)  #??
+  docker rm "$id"  #??
+  # Remove image
+  docker rmi "$image":latest  #??
+  # check removal
+  docker image ls |grep "$image"  #??
+  ;;
+2)
+  printf "Remove image %s" ${image}
+  # Remove image
+  docker rmi "$image":latest  #??
+  # check removal of image
+  docker image ls |grep "$image" #??
+  ;;
+*)
+  echo "Incorrect choice entered!"
+  ;;
+esac
